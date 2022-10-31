b8fa953f28ab853a2d685f1b4cb9f8fb3ecc2188
MattheusSantana <matheusess@hotmail.com>
2022-10-31 11:24:59 -0400

    User create method controller

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const userRoute = require("./src/routes/user.route");
-
+app.use(express.json());
 app.use('/user', userRoute);
 
 app.listen(3000);
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,18 @@
-const userLogin = (req, res) => {
-    res.json("Hello User");
+const create = (req, res) => {
+    const {name, username, email, password} = req.body;
+
+    if(!name || !username || !email || !password){
+        res.status(400).json({message: "Please submit all fields"})
+    }
+
+    res.status(201).json({
+        message: "User created successfully",
+        user: {
+            name,
+            email,
+            password
+        }
+    });
 }
 
-module.exports = { userLogin };
+module.exports = { create };
