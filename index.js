f32122600bc68d4adf316bb8dac2c62220614276
MattheusSantana <matheusess@hotmail.com>
2023-01-04 09:18:54 -0400

    Configurando variaveis de ambiente

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ import express from 'express';
 import userRoute from './src/routes/user.route.js';
 import connectMongoDB from './src/mongo/connect.js';
 import getElasticSearchClient from './src/client/elasticsearch.js';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
+const port = process.env.port || 3000;
 
 app.use(express.json());
 app.use('/user', userRoute);
@@ -26,4 +29,4 @@ connectMongoDB();
 
 
 
-app.listen(3000, () => console.log('server running'));
+app.listen(port, () => console.log('server running'));
--- src/mongo/connect.js
+++ src/mongo/connect.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
 
+
 const connectMongoDB = () => {
     console.log("wait connecting database");
-
-    mongoose.connect("mongodb+srv://root:root@cluster0.ywak5gt.mongodb.net/?retryWrites=true&w=majority", {
+    mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
